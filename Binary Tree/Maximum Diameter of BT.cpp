81fcdb04e2c9488d750487d302559b1a5de445d9
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2023-08-09 20:09:38 +0530

    diameter of a bt

---
+++ Binary Tree/Maximum Diameter of BT.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+	int depth(TreeNode *root, int &ans)
+	{
+		if (!root)
+		{
+			return 0;
+		}
+		int left = depth(root->left, ans);
+		int right = depth(root->right, ans);
+		ans = max(ans, left + right);
+		return 1 + max(left, right);
+	}
+
+	int diameterOfBinaryTree(TreeNode *root)
+	{
+		if (!root)
+		{
+			return 0;
+		}
+		int ans = -1;
+		depth(root, ans);
+		return ans;
+	}
+};
