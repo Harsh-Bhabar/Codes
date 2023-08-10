b1439632ab5868c88e4e37d22231dc86bea94cd7
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2023-08-10 19:34:31 +0530

    subtree of another tree

---
+++ Binary Tree/Subtree of another tree.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+	bool isSame(TreeNode *root, TreeNode *subRoot)
+	{
+		if (!root && !subRoot)
+		{
+			return true;
+		}
+		if (!root || !subRoot)
+		{
+			return false;
+		}
+		if (root->val != subRoot->val)
+		{
+			return false;
+		}
+		if (isSame(root->left, subRoot->left) && isSame(root->right, subRoot->right))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	bool isSubtree(TreeNode *root, TreeNode *subRoot)
+	{
+		if (!root || !subRoot)
+		{
+			return false;
+		}
+		if (isSame(root, subRoot))
+		{
+			return true;
+		}
+		if (isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot))
+		{
+			return true;
+		}
+		return false;
+	}
+};
