4fe9976b35c352e891fc5e072321d97133aa7205
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2023-08-11 21:37:18 +0530

    kth largest ele in a BST

--- Binary Search Tree/Kth Largest ele in a BST.cpp
+++ Binary Search Tree/Kth Largest ele in a BST.cpp
@@ -1,43 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution
 {
 public:
-	int kthSmallest(TreeNode *root, int k)
+	int kthLargest(Node *root, int k)
 	{
+		// Your code here
 		if (!root)
 		{
-			return 0;
+			return -1;
 		}
 
-		stack<TreeNode *> st;
-		TreeNode *prev = NULL;
+		stack<Node *> st;
 		while (root || !st.empty())
 		{
 			while (root)
 			{
 				st.push(root);
-				root = root->left;
+				root = root->right;
 			}
 			root = st.top();
 			st.pop();
 			if (k == 1)
 			{
-				return root->val;
+				return root->data;
 			}
-			root = root->right;
+			root = root->left;
 			k--;
 		}
 
-		return 0;
+		return -1;
 	}
 };
