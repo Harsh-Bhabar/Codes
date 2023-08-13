2ab071ca32a428c7fc165029d1c4108c19b5e6ee
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2023-08-13 21:35:24 +0530

    bst from postorder

--- Binary Search Tree/construct BST from postorder.cpp
+++ Binary Search Tree/construct BST from postorder.cpp
@@ -1,45 +1,37 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution
+/*struct Node
 {
-public:
-	TreeNode *insertInBST(TreeNode *head, int val)
-	{
-		if (!head)
-		{
-			return new TreeNode(val);
-		}
-		if (head->val < val)
-		{
-			head->right = insertInBST(head->right, val);
-		}
-		else
-		{
-			head->left = insertInBST(head->left, val);
-		}
-		return head;
-	}
+	int data;
+	Node *left, *right;
+};*/
 
-	TreeNode *bstFromPreorder(vector<int> &preorder)
+Node *insertInBST(Node *root, int val)
+{
+	if (!root)
 	{
-		if (preorder.size() == 0)
-		{
-			return NULL;
-		}
-		TreeNode *head = new TreeNode(preorder[0]);
-		for (int i = 1; i < preorder.size(); i++)
-		{
-			insertInBST(head, preorder[i]);
-		}
-		return head;
+		return new Node(val);
 	}
-};
+	if (root->data < val)
+	{
+		root->right = insertInBST(root->right, val);
+	}
+	else
+	{
+		root->left = insertInBST(root->left, val);
+	}
+	return root;
+}
+
+Node *constructTree(int post[], int size)
+{
+	// code here
+	if (size == 0)
+	{
+		return NULL;
+	}
+	Node *newHead = new Node(post[size - 1]);
+	for (int i = size - 2; i >= 0; i--)
+	{
+		insertInBST(newHead, post[i]);
+	}
+	return newHead;
+}
