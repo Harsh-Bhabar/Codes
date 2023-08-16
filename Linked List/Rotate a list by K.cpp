51c4066008b2cb63936e0d73e64b09434957756d
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2023-08-16 22:10:23 +0530

    rotate a list by K

--- Linked List/Rotate a list by K.cpp
+++ Linked List/Rotate a list by K.cpp
@@ -1,39 +1,42 @@
 /**
  * Definition for singly-linked list.
- * public class ListNode {
+ * struct ListNode {
  *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
 class Solution
 {
-public
-	ListNode rotateRight(ListNode head, int k)
+public:
+	ListNode *rotateRight(ListNode *head, int k)
 	{
-		if (head == null || head.next == null || k == 0)
-			return head;
-
-		ListNode cur = head;
-		int len = 1;
-		while (cur.next != null)
+		if (!head or !head->next || k == 0)
 		{
-			len++;
-			cur = cur.next;
+			return head;
 		}
 
-		cur.next = head;
-		k = k % len;
-		k = len - k;
-		while (k-- > 0)
-			cur = cur.next;
+		int len = 1;
+		ListNode *curr = head;
+		while (curr->next)
+		{ // till last node
+			len++;
+			curr = curr->next;
+		}
+		curr->next = head; // make it point to head
 
-		// make the node head and break connection
-		head = cur.next;
-		cur.next = null;
+		k = (len - k % len); // find new k
+
+		while (k--)
+		{ // move till kth node;
+			curr = curr->next;
+		}
+
+		head = curr->next; // make a new head;
+		curr->next = NULL; // mark it as end of list
 
 		return head;
 	}
-}
+};
