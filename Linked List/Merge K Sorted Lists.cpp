ed03d7b55b7765a2f918a19d8bfc7891e00c14ea
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2023-08-17 12:47:27 +0530

    merge k lists using merge sort

--- Linked List/Merge K Sorted Lists.cpp
+++ Linked List/Merge K Sorted Lists.cpp
@@ -1,4 +1,5 @@
-// using MINHEAP
+// using MINHEAP and making new list
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -36,5 +37,70 @@ public:
 		}
 
 		return newHead->next;
+	}
+};
+
+// using MERGE SORT
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+	ListNode *mergeTwoLL(ListNode *l1, ListNode *l2)
+	{
+		if (!l1)
+		{
+			return l2;
+		}
+		if (!l2)
+		{
+			return l1;
+		}
+		if (l1->val <= l2->val)
+		{
+			l1->next = mergeTwoLL(l1->next, l2);
+			return l1;
+		}
+		else
+		{
+			l2->next = mergeTwoLL(l1, l2->next);
+			return l2;
+		}
+		return NULL;
+	}
+
+	ListNode *breakAndJoin(vector<ListNode *> &lists, int l, int r)
+	{
+		if (l > r)
+		{
+			return NULL;
+		}
+		if (l == r)
+		{
+			return lists[l];
+		}
+		int mid = l + (r - l) / 2;
+		ListNode *l1 = breakAndJoin(lists, l, mid);
+		ListNode *l2 = breakAndJoin(lists, mid + 1, r);
+		return mergeTwoLL(l1, l2);
+	}
+
+	ListNode *mergeKLists(vector<ListNode *> &lists)
+	{
+		int k = lists.size();
+		if (k == 0)
+		{
+			return NULL;
+		}
+		return breakAndJoin(lists, 0, k - 1);
 	}
 };
