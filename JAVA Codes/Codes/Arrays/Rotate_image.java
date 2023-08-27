c4cdbcb70c45a5c8393850b8ae3629d904033ac4
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2023-08-27 22:07:41 +0530

    rotate image

---
+++ JAVA Codes/Codes/Arrays/Rotate_image.java
@@ -0,0 +1,32 @@
+package Codes.Arrays;
+
+public class Rotate_image {
+	
+}
+
+class Solution {
+    public void rotate(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<i; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            int left = 0;
+            int right = n - 1;
+            while (left < right) {
+                int temp = mat[i][left];
+                mat[i][left] = mat[i][right];
+                mat[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
